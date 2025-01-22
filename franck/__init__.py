6197d52c7533d02ac55eb6544471c02db1994620
DAMIE Marc <marc.damie@inria.fr>
2025-01-22 17:55:54 +0100

    Fix the init file

--- franck/__init__.py
+++ franck/__init__.py
@@ -2,7 +2,7 @@ from .common import Crawler
 from .friendica import FriendicaFederationCrawler
 from .lemmy_crawler import LemmyCommunityCrawler, LemmyFederationCrawler
 from .mastodon_crawler import MastodonFederationCrawler
-from .misskey_crawler import MisskeyTopUserCrawler
+from .misskey_crawler import MisskeyActiveUserCrawler, MisskeyFederationCrawler
 from .peertube_crawler import PeertubeCrawler
 from .pleroma_crawler import PleromaActiveUserCrawler, PleromaFederationCrawler
 
