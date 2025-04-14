71ecde69d56fd8c7d85dddd0fe427e1582d64fba
DAMIE Marc <marc.damie@inria.fr>
2025-04-14 11:25:53 +0200

    Bump the package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="franck",
-    version="1.0.1",
+    version="1.0.2",
     url="https://github.com/MarcT0K/Franck",
     author="Marc DAMIE",
     author_email="marc.damie@inria.fr",
